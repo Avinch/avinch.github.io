53559525188ca023039dd52d28f67a538348849a
Sergestra <obuar93@gmail.com>
2022-01-19 01:06:52 -0800

    feat: add general update to scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,29 @@
-// Scripts
+// Enable strict mde
+'use strict';
+
+// Initialize AOS
+AOS.init();
+AOS.init({
+  duration: 1000
+});
+
+//
+const exploreBtn = document.getElementById('exploreBtn');
+const scrollHere = document.getElementById('scrollHere');
+
+exploreBtn.addEventListener('click', (e) => {
+  scrollHere.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+$("#exploreBtn").click(function () {
+  $('html, body').animate({
+    scrollTop: $("#scrollHere").offset().top
+  }, 1400);
+});
+
+VanillaTilt.init({
+  max: 15,
+  reset: false
+});
